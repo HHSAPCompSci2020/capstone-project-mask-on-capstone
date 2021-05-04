b5cacafabe92aaf557d747add94c9a5ea844c7e7
Emily Tumacder <EmilyTumacder@Emilys-MBP.attlocal.net>
2021-05-03 18:13:52 -0700

    fake push bc github doesn't like me

---
+++ Mask On/src/display/Location.java
@@ -0,0 +1,9 @@
+package display;
+
+import java.util.ArrayList;
+
+public class Location {
+
+	private int row, col;
+	private ArrayList<GameComponent> gameComponents;
+}
--- Mask On/src/display/Tier.java
+++ Mask On/src/display/Tier.java
@@ -1,11 +1,15 @@
 package display;
 
 import java.awt.Color;
+import java.util.ArrayList;
+
 import processing.core.PApplet;
 
 public abstract class Tier extends Display {
 	private int infectedPeople;
 	private int totalPeople;
+	static Location[][] grid;
+	
 	public Tier(double x, double y, double width, double height, int strokeWeight, Color strokeColor, Color fillColor, int totalPeople) {
 		super(x, y, width, height, strokeWeight, strokeColor, fillColor);
 		// TODO Auto-generated constructor stub
@@ -18,4 +22,5 @@ public abstract class Tier extends Display {
 	public int getTotal() {
 		return totalPeople;
 	}
+	public 
 }
